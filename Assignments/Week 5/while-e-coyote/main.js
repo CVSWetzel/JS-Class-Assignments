d2f5f951fde363627b15677e69dc7c12a7658836
Chynna Wetzel <cwetazel.prsvr@gmail.com>
2024-04-11 23:04:35 -0400

    while-e-coyote

--- Assignments/Week 5/while-e-coyote/main.js
+++ Assignments/Week 5/while-e-coyote/main.js
@@ -1,17 +1,29 @@
-function numberDoubler(num) {
-    // Your code here
-}
-
-function stringRepeater(str) {
-    // Your code here
-}
-
-function makeDivisible(x, y) {
-    // Your code here
-}
-
-module.exports = {
+const numberDoubler = (num) => {
+    while (num < 100) {
+      num *= 2;
+    }
+    return num;
+  };
+  
+  const stringRepeater = (str) => {
+    // Calculate the number of repetitions needed to reach or exceed 10 characters
+    const repetitions = Math.ceil(10 / str.length);
+    // Repeat the string the calculated number of times
+    return str.repeat(repetitions);
+  };
+  
+  
+  
+   const makeDivisible = (x, y) => {
+    while (x % y !== 0) {
+      x++;
+    }
+    return x;
+  };
+  
+  module.exports = {
     numberDoubler,
     stringRepeater,
-    makeDivisible
-};
+    makeDivisible,
+  };
+  
