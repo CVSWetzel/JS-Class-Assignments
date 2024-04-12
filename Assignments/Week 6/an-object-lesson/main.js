b8ca27a1a5e831a7dcc452cb7ad30b0f1e6fcfd8
Chynna Wetzel <cwetazel.prsvr@gmail.com>
2024-04-11 23:23:05 -0400

    an-object-lesson

--- Assignments/Week 6/an-object-lesson/main.js
+++ Assignments/Week 6/an-object-lesson/main.js
@@ -11,54 +11,65 @@ myPersevereCodeObj.wait= 'I object'
 console.log(myPersevereCodeObj.wait)
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-
+myPersevereCodeObj.isAnExcellentObject = true;
 
 // Add the property `term` and set it to 1.
-
+myPersevereCodeObj.term = 1;
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-
+myPersevereCodeObj.term += 1;
 
 // Now add a new property called `currentScore` and set it to 3.
-
+myPersevereCodeObj.currentScore = 3;
 
 // Now add a new property called `cumulativeScore` and set it to 50.
-
+myPersevereCodeObj.cumulativeScore = 50;
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-
+myPersevereCodeObj.cumulativeScore += myPersevereCodeObj.currentScore;
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
-
+myPersevereCodeObj['I am exactly this cool'] = 'soooooo cool';
 
 // Part 2 - your own object.
 // Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
-
+let myObject = {};
 
 // Add a `firstName` property and set it to a string.
-
+myObject.firstName = 'Chynna';
 
 // Add a `lastName` property and set it to a string.
-
+myObject.lastName = 'Wetzel';
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
-
+myObject.fullName = myObject.firstName + ' ' + myObject.lastName;
 
 // Add an `age` property and set it to a number.
-
+myObject.age = 31;
 
 // Add an address property and set it to an empty array.
-
+myObject.address =[];
 
 // Now push three strings into that array.
-
+myObject.address.push('1234 Walnut St');
+myObject.address.push('Apt 563');
+myObject.address.push('Muscatine, IA');
 
 
 
 // Part 3 - setting key-value pairs in-line.
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `myPersevereCodeObj` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of something. The important thing here is: this should all be done within the curly brackets of the object, not below the place where the object is declared.
 
-
-
+const newObject = {
+  stringProperty: 'Hello',
+  booleanProperty: true,
+  numberProperty: 42,
+  arrayProperty: ['apple', 'banana', 'cherry'],
+  'property with space': 'value',
+  'property-with-dash': 'value',
+  '1stProperty': 'value',
+};
 
 // As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
+
+
